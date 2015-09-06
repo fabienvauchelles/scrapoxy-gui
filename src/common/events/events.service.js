5f5cf057f690cab15f8c61eb49d16876d0287fff
VAUCHELLES Fabien <fabien@vauchelles.com>
2015-09-06 23:57:26 +0200

    feat(event): add debug logging on socket.io

--- src/common/events/events.service.js
+++ src/common/events/events.service.js
@@ -10,7 +10,7 @@
         .module('myApp')
         .factory('EventsService', EventsService);
 
-    function EventsService($q, $rootScope) {
+    function EventsService($q, $log, $rootScope) {
         var socket;
 
         var factory = {
@@ -24,6 +24,8 @@
         ////////////
 
         function start() {
+            $log.debug('[EventService] start');
+
             return new $q(function (resolve, reject) {
                 if (!window.io) {
                     reject(new Error('Cannot find socket.io'));
@@ -40,6 +42,8 @@
                     socket.on('event', function (data) {
                         var data = JSON.parse(data);
 
+                        $log.debug("[EventService] event '%s': ", data.event, data.payload);
+
                         $rootScope.$apply(function () {
                             $rootScope.$emit(data.event, data.payload);
                         });
@@ -54,6 +58,8 @@
         }
 
         function stop() {
+            $log.debug('[EventService] stop');
+
             if (!socket) {
                 return;
             }
